a336c595fd2ea2bb07f4abdf1f451ba04777b8fe
Joe Polny <joepolny@gmail.com>
2026-02-05 10:10:06 -0500

    initial commit

---
+++ oracle/src/index.ts
@@ -0,0 +1,20 @@
+import { ed25519 } from "@noble/curves/ed25519.js";
+import { DstackClient } from "@phala/dstack-sdk";
+import express from "express";
+import "dotenv/config";
+
+const client = new DstackClient("../dstack/sdk/simulator/dstack.sock");
+const key = ed25519.keygen();
+await client.emitEvent("pubkey", key.publicKey);
+
+const app = express();
+const PORT = 3000;
+
+app.get("/quote", async (_, res) => {
+  const quote = await client.getQuote("my-custom-data");
+  res.json(quote);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
